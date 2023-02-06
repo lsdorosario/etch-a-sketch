6c4326bd52ec49a91aa3dc7a7b56e62d719a0408
Lucas do Rosario <lsdorosario@gmail.com>
2023-02-06 13:42:22 -0300

    Add loop to create 16x16 grid

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ const gridContainer = document.getElementById("grid-container");
 const gridHeight = gridContainer.offsetHeight;
 const gridWidth = gridContainer.offsetWidth;
 
-
+// Creates one block
 function createBlock() {
     const gridBlock = document.createElement("div");
     gridBlock.style.height = `${gridHeight / 16}px`;
     gridBlock.style.width = `${gridHeight / 16}px`;
-    gridBlock.style.border = "1px solid black";
     gridContainer.appendChild(gridBlock);
 }
 
-createBlock();
+// Loops 256 times to create a 16x16 grid (16²)
+for (let i = 0; i < 256; i++) {
+    createBlock();
+}
