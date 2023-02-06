e023ed145fdbfa0bf5a48a216caa3b07e2cf093d
Lucas do Rosario <lsdorosario@gmail.com>
2023-02-06 10:56:55 -0300

    Fix height and width on createBlock function

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ const gridContainer = document.getElementById("grid-container");
 const gridHeight = gridContainer.offsetHeight;
 const gridWidth = gridContainer.offsetWidth;
 
-function createGridBlock() {
-    let gridBlock = document.createElement("div");
-    gridBlock.style.height = "100px"
-    gridBlock.style.width = "100px"
+
+function createBlock() {
+    const gridBlock = document.createElement("div");
+    gridBlock.style.height = `${gridHeight / 16}px`;
+    gridBlock.style.width = `${gridHeight / 16}px`;
     gridBlock.style.border = "1px solid black";
     gridContainer.appendChild(gridBlock);
 }
 
-createGridBlock();
+createBlock();
